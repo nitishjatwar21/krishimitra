e589cc77516b6140b4f8358eeb49c15896accc32
AJAY KUMAR <48375037+AJAYKR00KJ@users.noreply.github.com>
2020-09-30 21:08:23 +0530

    Added models for landlord and farmer

--- landlord/models.py
+++ landlord/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+class about(models.Model):
+    name = models.CharField(max_length=20)
+    dob = models.DateTimeField()
+    college =models.CharField(max_length=30)
+    cgpa = models.IntegerField(default=0)
+    about= models.TextField(max_length=50)
+    profile = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+ 
+    def __str__(self):
+        return self.name
---
+++ supplier/migrations/__init__.py
@@ -0,0 +1,30 @@
+
+# from django.db import models
+
+# # Create your models here.
+# class Post(models.Model):
+#     title = models.TextField()
+#     cover = models.ImageField(upload_to='images/')
+
+#     def __str__(self):
+#         return self.title
+
+# class about(models.Model):
+#     name = models.CharField(max_length=20)
+#     dob = models.DateTimeField()
+#     college =models.CharField(max_length=30)
+#     cgpa = models.IntegerField(default=0)
+#     about= models.TextField(max_length=50)
+#     profile = models.ImageField(upload_to='images/')
+
+#     def __str__(self):
+#         return self.name
+
+# class log(models.Model):
+#     email=models.EmailField()
+#     passw = models.CharField(max_length=20)
+#     name= models.CharField(max_length=20)
+
+#     def __str__(self):
+#         return self.name
+#
